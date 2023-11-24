f561a2e83a1cdbf84ad894b6a9737b027c637e23
Ho Nyeong <dev.honing@gmail.com>
2023-11-24 23:07:52 +0900

    css 적용해보려 하니 loader 설치가 필요했다.

---
+++ src/HelloWorld.js
@@ -0,0 +1,13 @@
+// src/HelloWorld.js
+
+import React from 'react';
+
+const HelloWorld = () => {
+  return (
+    <div>
+      <h2>Hello, World! This is a new component.</h2>
+    </div>
+  );
+};
+
+export default HelloWorld;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import HelloWorld from './HelloWorld';
+import './styles.css'; // 추가된 부분
 
 const App = () => {
   return (
     <div>
       <h1>Hello, React!</h1>
+      <HelloWorld />
     </div>
   );
 };
