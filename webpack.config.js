a70a6ea7be03d86aadccae1ec06c822627c5cfd5
Ho Nyeong <dev.honing@gmail.com>
2023-11-24 22:51:46 +0900

    Hello, React! 출력 성공

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -25,4 +26,11 @@ module.exports = {
     port: 3000,
     open: true,
   },
+  mode: 'development', // 또는 'production'
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html', // 기존의 index.html 파일 경로
+      filename: 'index.html', // 생성될 파일명
+    }),
+  ],
 };
